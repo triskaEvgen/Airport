93db19a9b04a17d25b8e12f6704c534abf6c6295
vetalolon <vetalolon@gmail.com>
2018-07-21 10:59:40 +0300

    Init

---
+++ src/main/java/airlines/App.java
@@ -0,0 +1,17 @@
+package airlines;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class App 
+{
+    public static void main( String[] args )  {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("mysql");
+        EntityManager em = factory.createEntityManager();
+        
+        
+        em.close();
+        factory.close();
+    }
+}
